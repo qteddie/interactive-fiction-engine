1066a4d45c086e5d88c640885c4f950fdd8da1b8
林子軒 Lin, Tzu-Hsuan <qteddie@gmail.com>
2024-04-19 21:49:55 +0800

    update

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 all:
-	gcc -I./libs/tomlc99 src/main.c ./libs/tomlc99/toml.c -o main
+	gcc -I./libs/tomlc99 -I./libs/jansson/src src/main.c src/simple_parse.c ./libs/tomlc99/toml.c -o main
 clean:
 	rm -f main
--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
-#include "toml.h"
-
+#include <jansson.h>
+#include <toml.h>
 static void error(const char* msg, const char* msg1)
 {
     fprintf(stderr, "ERROR: %s%s\n", msg, msg1?msg1:"");
@@ -13,50 +13,28 @@ static void error(const char* msg, const char* msg1)
 
 int main()
 {
-    FILE* fp;
-    char errbuf[200];
+    toml_table_t* server = toml_table_in(root, "server");
+    if (server) {
+        toml_datum_t host = toml_string_in(server, "host");
+        if (host.ok) {
+            json_object_set_new(root, "host", json_string(host.u.s));
+            free(host.u.s);
+        }
 
-    // 1. Read and parse toml file
-    fp = fopen("static/toml/sample.toml", "r");
-    if (!fp) {
-        error("cannot open sample.toml - ", strerror(errno));
+        toml_array_t* port = toml_array_in(server, "port");
+        if (port) {
+            json_t* port_array = json_array();
+            for (int i = 0; i < toml_array_nelem(port); i++) {
+                toml_datum_t p = toml_int_at(port, i);
+                if (p.ok) {
+                    json_array_append_new(port_array, json_integer(p.u.i));
+                }
+            }
+            json_object_set_new(root, "port", port_array);
+        }
     }
 
-    toml_table_t* conf = toml_parse_file(fp, errbuf, sizeof(errbuf));
-    fclose(fp);
-
-    if (!conf) {
-        error("cannot parse - ", errbuf);
-    }
-
-    // 2. Traverse to a table.
-    toml_table_t* server = toml_table_in(conf, "server");
-    if (!server) {
-        error("missing [server]", "");
-    }
-
-    // 3. Extract values
-    toml_datum_t host = toml_string_in(server, "host");
-    if (!host.ok) {
-        error("cannot read server.host", "");
-    }
-
-    toml_array_t* portarray = toml_array_in(server, "port");
-    if (!portarray) {
-        error("cannot read server.port", "");
-    }
-
-    printf("host: %s\n", host.u.s);
-    printf("port: ");
-    for (int i = 0; ; i++) {
-        toml_datum_t port = toml_int_at(portarray, i);
-        if (!port.ok) break;
-        printf("%d ", (int)port.u.i);
-    }
-    printf("\n");
-
-    // 4. Free memory
-    free(host.u.s);
-    toml_free(conf);
+    json_dump_file(root, "static/json/output.json", JSON_INDENT(4));
     return 0;
+
 }
